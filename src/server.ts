10a5586e74752654d079a84f7fbf749f96dbe8ce
RicardoMarassato <ricardo.marassato@gmail.com>
2021-05-27 18:54:18 -0300

    Add swagger doc to project

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import { app } from ".";
+import express, { json, Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../src/swagger.json";
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(Router());
 
 app.listen(3333, () => console.log("Server is running!"));
+
+
